f8f3ccd94ceaa82ed776e6ba9a032a5c42680a6b
raywalz <33192362+raywalz@users.noreply.github.com>
2020-12-19 00:19:29 -0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/four_words/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.four_words
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'eff_short_wordlist_1.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Four Words',
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        body: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RandomWord(),
+        RandomWord(),
+        RandomWord(),
+        RandomWord(),
+      ],
+    ));
+  }
+}
+
+class RandomWord extends StatefulWidget {
+  @override
+  _RandomWordState createState() => _RandomWordState();
+}
+
+class _RandomWordState extends State<RandomWord> {
+  String randomWord = 'touch me';
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text(
+        randomWord,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 48.0,
+        ),
+      ),
+      onPressed: () {
+        setState(() {
+          randomWord = wordList[Random().nextInt(wordList.length)];
+        });
+      },
+    );
+  }
+}
